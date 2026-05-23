e27c484f699c15b3b55c277e982146f7e01c7953
Adeelabbas5 <syedadeelabbas151@gmail.com>
2026-05-23 18:22:57 +0500

    uppppp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,13 +15,6 @@ $app = Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {})
-    ->create();
-
-// Force Vercel to use the writable /tmp directory
-if (isset($_ENV['VERCEL'])) {
-    $app->useStoragePath('/tmp/storage');
-    $app['config']->set('view.compiled', '/tmp/storage/framework/views');
-}
-
-return $app;
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
