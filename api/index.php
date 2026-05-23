9fd874193ef24569bd9394748f2ba7b1ecf0b60e
Adeelabbas5 <syedadeelabbas151@gmail.com>
2026-05-23 18:49:06 +0500

    uppppp

--- api/index.php
+++ api/index.php
@@ -2,14 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// 1. Boot the application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+// 2. Handle the request directly using the Laravel 11 bootstrapper
+$response = $app->handleRequest(
+    Illuminate\Http\Request::capture()
 );
 
+// 3. Send the response
 $response->send();
-
-$kernel->terminate($request, $response);
