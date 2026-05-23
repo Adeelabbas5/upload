8c559220e2eeb7ae4df0cf7be280574055bd076e
Adeelabbas5 <syedadeelabbas151@gmail.com>
2026-05-23 18:20:35 +0500

    uppppp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. Initialize the Application
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,13 +15,11 @@ $app = Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
+    ->withExceptions(function (Exceptions $exceptions): void {})
     ->create();
 
-// 2. Early-bind storage paths for Vercel
-if (isset($_ENV['VERCEL_JOB_ID']) || isset($_SERVER['VERCEL_COMMIT_ID'])) {
+// Force Vercel to use the writable /tmp directory
+if (isset($_ENV['VERCEL'])) {
     $app->useStoragePath('/tmp/storage');
     $app['config']->set('view.compiled', '/tmp/storage/framework/views');
 }
