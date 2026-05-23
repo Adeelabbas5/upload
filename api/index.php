d8ffe56a3ac0c5a9609db6fbe61abebe6f66597d
Adeelabbas5 <syedadeelabbas151@gmail.com>
2026-05-23 18:52:19 +0500

    uppppp

--- api/index.php
+++ api/index.php
@@ -2,13 +2,15 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// 1. Boot the application
+// Boot the application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 2. Handle the request directly using the Laravel 11 bootstrapper
+// FORCE: Ensure the application is fully booted
+// This is critical for Serverless to initialize the View service
+$app->boot();
+
 $response = $app->handleRequest(
     Illuminate\Http\Request::capture()
 );
 
-// 3. Send the response
 $response->send();
