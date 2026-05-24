2395dbaec78e5def35ffc9f993df1a2820bd5279
Adeelabbas5 <syedadeelabbas151@gmail.com>
2026-05-24 22:04:34 +0500

    hoja bhai ab please

--- config/app.php
+++ config/app.php
@@ -121,9 +121,6 @@ return [
          */
 
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
 
     ],
 
